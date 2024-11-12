ea05bac5e0fd3a4ecee75509b7de554f424e5b2b
kauzin77 <151050370+kauzin77@users.noreply.github.com>
2024-11-11 21:33:03 -0300

    1

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function verificador(){
-    const numero = document.getElementById('numero').value;
-    const resultado = document.getElementById('resultado');
+function verificar(){
+    var n1 = document.getElementById('nota1').value;
+    var n2 = document.getElementById('nota2').value;
+    var n3 = document.getElementById('nota3').value;
+    var resultado = document.getElementById('resultado');
+    var media = (parseFloat(n1)+parseFloat(n2)+parseFloat(n3))/3;
 
-    if(numero % 2 === 0){
-        resultado.textContent = "Este número é par.";
+    if (media >= 6){
+        resultado.textContent = "Parabéns! Aprovado com média " + media.toFixed(1) +".";
     } else {
-        resultado.textContent = "Este número é ímpar.";
+        resultado.textContent = "Reprovado com média " + media.toFixed(1) + ".";
     }
 }
